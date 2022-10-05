b70a8ee8d7a4a32dc24a54f56f8ed49ea3622caf
andrews-zack <zachary.andrews.au19@gmail.com>
2022-10-05 19:54:52 +0000

    first test of state objects

--- sandbox/sandbox.js
+++ sandbox/sandbox.js
@@ -1,1 +1,34 @@
 // State objects
+let boardState = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
+
+let winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+let playerTurn = {
+    0: init,
+    1: "X",
+    2: "O",
+    3: gameEnd,
+};
+
+function init() {
+
+};
